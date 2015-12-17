ae112eaa337044b612525397ec4b3ccabb69980c
Melany Gulianovych <gmr@inbox.ru>
2015-12-17 13:15:45 +0200

    Initial Commit

---
+++ MyPaint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPaint
+//
+//  Created by Melany Gulianovych on 12/17/15.
+//  Copyright (c) 2015 Melany Gulianovych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
